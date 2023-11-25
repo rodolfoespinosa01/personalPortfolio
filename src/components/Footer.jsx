eb02185ad38ddd89196216f26658c6af52cd43ce
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-25 13:03:59 -0500

    Footer is much more responsive. Icons change flex-direction based on screen size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,31 +8,31 @@ function Footer() {
   const date = new Date()
 
   return (
-    <footer className="w-full bg-[#003554] flex flex-row justify-between items-center p-5 text-slate-300">
+    <footer className="w-full h-80 bg-[#003554] flex flex-col justify-between items-center p-5 text-slate-300">
 
       <p>&copy; {date.getFullYear()}</p>
 
-      <ul className="flex">
+      <ul className="flex flex-col md:flex-row md:space-x-20">
 
-        <li className="w-[120px] h-[60px] rounded  flex justify-between items-center bg-[#003554]">
+        <li className="w-[120px] h-[40px] rounded  flex justify-between items-center bg-[#006494]">
           <a className="flex justify-between items-center w-full text-gray-300" href="/resume">Resume <BsFillPersonLinesFill size={30} /></a>
         </li>
 
-        <li className="w-[120px] h-[60px] rounded flex justify-between items-center bg-blue-600">
+        <li className="w-[120px] h-[40px] rounded flex justify-between items-center bg-blue-600">
           <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/rodolfo-espinosa-nunez-77131b64/" target="_blank" rel="noopener noreferrer">Linkedin <FaLinkedin size={30} /></a>
         </li>
 
-        <li className="w-[120px] h-[60px] rounded flex justify-between items-center bg-[#333333]">
+        <li className="w-[120px] h-[40px] rounded flex justify-between items-center bg-[#333333]">
           <a className="flex justify-between items-center w-full text-gray-300" href="https://github.com/rodolfoespinosa01" target="_blank" rel="noopener noreferrer">Github <FaGithub size={30} /></a>
         </li>
 
-        <li className="w-[120px] h-[60px] rounded flex justify-between items-center bg-[#0582ca]">
+        <li className="w-[120px] h-[40px] rounded flex justify-between items-center bg-[#0582ca]">
           <a className="flex justify-between items-center w-full text-gray-300" href="/contact">Contact <HiOutlineMail size={30} /></a>
         </li>
 
       </ul>
 
-      <p>Developed by Rodolfo E.N</p>
+      <p className="mb-8">Developed by Rodolfo E.N</p>
     </footer>
   )
 }
