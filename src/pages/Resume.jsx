2e24effb68c7b15997f6701b1006b27c707c7ac1
Rodolfo <rodolfoespinosa01@gmail.com>
2023-12-01 23:01:07 -0500

    Shortened summary on Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -27,16 +27,14 @@ const Resume = () => {
 
   return (
     <>
-      <div className="w-full h-screen bg-[#051923] flex flex-col justify-center items-center p-4 text-white">
+      <div className="w-full h-auto bg-[#051923] flex flex-col justify-center items-center p-4 text-white">
         <div className="mb-8 text-center">
           <p className="lg:text-4xl text-2xl font-bold">Rodolfo Espinosa-Nunez</p>
           <p className="lg:text-3xl text-xl text-[#006494]">Full Stack Developer</p>
         </div>
 
         <div className="mb-8 text-center">
-          <p className="max-w-xl p-15">
-            In brief, I previously managed a profitable business providing online coaching fitness services to fitness influencers who offered tailor-made diet and training plans for their clients. Now, I've innovated a method to automate the customization of diets. My goal is to utilize my recently acquired Full Stack Web Development skills not only to construct my personal app but also to actively seek employment in web development. Simultaneously, I aim to develop my app as a side project to stay engaged, enhance my skills, and apply the acquired knowledge to my prospective job in web development.
-          </p>
+          <p className="max-w-xl p-15">Formerly ran a successful fitness coaching business, now leveraging Full Stack Web Development skills to automate diet customization. Seeking web development employment while building a personal app as a side project to enhance skills and apply knowledge to future roles.</p>
         </div>
 
         <div>
@@ -49,7 +47,7 @@ const Resume = () => {
         </div>
 
         <div>
-          <img className="lg:w-64 w-32 m-10" src={smileEmoji} alt="" />
+          <img className="lg:w-64 w-32 m-10" src={smileEmoji} alt="smiling emoji" />
         </div>
 
       </div>
